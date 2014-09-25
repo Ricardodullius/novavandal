10efe9ccf0705067a56b81da20f91363c844aacc
Ricardo Dullius <ricardodullius@gmail.com>
2014-09-25 16:28:30 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Novavandal::Application.config.secret_key_base = '3f8ee77cebeb9fcdd1a79c50f225ac4391e9204ac3d4c90603563e62f62c54d7fdb3afaff06467dc9f6f047fa139ca04210dfd6f3b3940b77efff78140c77c45'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Novavandal::Application.config.session_store :cookie_store, key: '_novavandal_session'
